fb54cc9050833e555590e0044af48c09093dc2d2
Ming <642203604@qq.com>
2016-10-19 23:38:49 +0800

    加入线程池进行并发控制

--- src/com/mingslife/matrix/app/Main.java
+++ src/com/mingslife/matrix/app/Main.java
@@ -2,6 +2,8 @@ package com.mingslife.matrix.app;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.mingslife.matrix.core.Logger;
 import com.mingslife.matrix.core.Receiver;
@@ -10,14 +12,17 @@ public class Main {
 	private static Logger logger = new Logger(Main.class);
 
 	private static int SERVER_PORT = 3000;
+	private static int THREAD_NUMBERS = 100;
 
 	public void start() throws Exception {
 		ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+		ExecutorService executor = Executors.newFixedThreadPool(THREAD_NUMBERS);
 		logger.info("Server is started on " + SERVER_PORT + ".");
 		while (true) {
 			Socket socket = serverSocket.accept();
 			Receiver receiver = new Receiver(socket);
-			new Thread(receiver).start();
+//			new Thread(receiver).start();
+			executor.execute(receiver);
 		}
 	}
 
